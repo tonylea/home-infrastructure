723b951e63e76c466819e028769a7b7d127c2b66
Tony Lea <tonylea@users.noreply.github.com>
2025-07-28 10:25:54 +0100

    chore: fix terraform

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -1,25 +1,29 @@
 terraform {
-  backend "remote" {
+  cloud {
     organization = "deadrobot"
 
     workspaces {
       name = "cloudflare"
     }
   }
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "5.7.1"
+    }
+  }
+
 }
 
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-data "cloudflare_zone" "domain" {
-  name = var.public_domain
-}
-
-resource "cloudflare_record" "traefik" {
-  zone_id = data.cloudflare_zone.domain.id
-  name    = "traefik"
-  value   = var.public_domain
+resource "cloudflare_dns_record" "traefik" {
+  zone_id = var.cloudflare_zone_id
+  name    = "traefik.${var.public_domain}"
+  content = var.cname_target
+  ttl     = 1
   type    = "CNAME"
   proxied = true
 }
--- terraform/cloudflare/variables.tf
+++ terraform/cloudflare/variables.tf
@@ -3,6 +3,15 @@ variable "cloudflare_api_token" {
   sensitive = true
 }
 
+variable "cloudflare_zone_id" {
+  type = string
+}
+
 variable "public_domain" {
   type = string
+}
+
+variable "cname_target" {
+  description = "The DNS target for the CNAME record (usually your Cloudflare Tunnel endpoint)"
+  type        = string
 }
