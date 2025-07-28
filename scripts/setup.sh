d97900a46ec70743a44761282902438cff0d504d
Tony Lea <tonylea@users.noreply.github.com>
2025-07-28 10:45:09 +0100

    ci: update setup.sh to remove install of brew

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Install Homebrew if not present
-if ! command -v brew &>/dev/null; then
-  echo "Installing Homebrew..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# Install tools
 brew install helm terraform ansible kubectl
